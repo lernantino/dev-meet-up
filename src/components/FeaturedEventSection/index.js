32940de70b9ec4cb7d5df361501b0505e98d24ab
sevans924 <sevans924@gmail.com>
2021-06-17 15:16:35 -0400

    first commit

---
+++ src/components/FeaturedEventSection/index.js
@@ -0,0 +1,55 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+import styles from './featuredEventSection.module.scss';
+
+const defaultEvent = {
+  slug: '/',
+  featuredImage: { url: '/images/featured-4.jpg' },
+  name: 'No featured event at the moment!',
+};
+
+const FeaturedEventSection = ({ event = { ...defaultEvent } }) => {
+  return (
+    <section className={styles.featuredEventSection}>
+      <div className={styles.featuredEventSection__grid}>
+        <Link href={`/events/${event.slug}`}>
+          <a className={styles.featuredEventSection__event}>
+            <Image
+              className={styles['featuredEventSection__event-image']}
+              src={event.featuredImage.url}
+              layout="fill"
+              objectFit="cover"
+              objectPosition="center"
+            />
+            <div className={styles['featuredEventSection__event-details']}>
+              <span>
+                <h2>Featured Event</h2>
+                <h3>{event.name}</h3>
+                {event.eventDate && (
+                  <h4>
+                    {new Intl.DateTimeFormat('en-US', {
+                      dateStyle: 'full',
+                    }).format(new Date(event.eventDate))}
+                  </h4>
+                )}
+              </span>
+            </div>
+          </a>
+        </Link>
+        <Link href="/events">
+          <a className={styles.featuredEventSection__callout}>
+            <span>See all events.</span>
+          </a>
+        </Link>
+        <Link href="/donate">
+          <a className={styles.featuredEventSection__callout}>
+            <span>Donate and keep the meetups coming!</span>
+          </a>
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export default FeaturedEventSection;
